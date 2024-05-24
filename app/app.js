d838bec17e6e385deb5f874dfbd7aaec490de012
Jatin Rathore <sbi.kindle.aadhaar.jatin@gmail.com>
2024-05-24 11:57:18 +0530

    commit 3

--- app/app.js
+++ app/app.js
@@ -1,3 +1,24 @@
 const fastify = require("fastify");
+const mongoose = require("mongoose");
 
 const app = fastify();
+
+mongoose
+  .connect("mongodb://localhost:27017/PROJECT_2_DB")
+  .then(() => {
+    console.info("database connected successfully");
+  })
+  .catch((err) => {
+    console.error("error in database connection : ", err);
+    process.exit(1);
+  });
+
+app.get("/", (request, reply) => {
+  reply.code(200);
+
+  return {
+    message: "server is up and running",
+  };
+});
+
+module.exports = app;
